e89d2d4f90fa201fd6c5f8bd8b70e3b63953649b
Georgina Sanchez Buenrostro <cpgeorgina.sanchez@gmail.com>
2019-09-03 20:31:26 -0700

    changed the name of the .all method to .create_all

--- lib/room.rb
+++ lib/room.rb
@@ -1,9 +1,23 @@
 module Hotel
   class Room
     attr_reader :room_num, :reservations
-    def initialize(room_num:, reservations: nil)
+    def initialize(room_num, reservations: nil)
       @room_num = room_num
       @reservations = reservations || []
     end
+
+    #creates and returns an array of all the rooms of the hotel
+    def self.create_all
+      rooms = []
+      room_num = 1
+      until room_num == 21
+        new_room = Room.new(room_num)
+        rooms << new_room
+        room_num += 1
+      end
+      return rooms
+    end
+
+
   end
 end
--- test/room_test.rb
+++ test/room_test.rb
@@ -1,16 +1,36 @@
 require_relative "test_helper"
 
-describe 'Room' do
-  describe '#initialize' do
+describe "Room" do
+  describe "#initialize" do
     before do
-      @room = Hotel::Room.new(
+      room = Hotel::Room.new(
         room_num: 1,
       )
     end
 
     it "is an instance of Room" do
-      expect(@room).must_be_kind_of Hotel::Room
+      expect(room).must_be_kind_of Hotel::Room
     end
   end
+
+  describe "Room.all" do
+    # before do 
+    #   rooms = Hotel::Room.all
+    # end
+
+    it "returns an array of all rooms" do
+      rooms = Hotel::Room.create_all
+      expect(rooms).must_be_kind_of Array
+    end
+
+    it "creates an instance for each room" do
+      rooms = Hotel::Room.create_all
+      expect(rooms.length).must_equal 20
+      rooms.each do |r|
+        expect(r).must_be_kind_of Hotel::Room
+      end
+    end
+  end
+
 end
 
