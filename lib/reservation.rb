2ba525d8c41bdcfc112282551f796e897705ccb6
Georgina Sanchez Buenrostro <cpgeorgina.sanchez@gmail.com>
2019-09-04 16:48:39 -0700

    gives the cost of a reservation

--- lib/reservation.rb
+++ lib/reservation.rb
@@ -1,11 +1,16 @@
 module Hotel
   class Reservation
     attr_reader :room_num, :start_date, :end_date
+
     def initialize(room_num, start_date, end_date)
       @room_num = room_num
       @start_date = Date.parse(start_date)
       @end_date = Date.parse(end_date)
       
+    end
+
+    def cost
+      return (@end_date - @start_date) * 200
     end
     
   end
--- test/reservation_test.rb
+++ test/reservation_test.rb
@@ -8,8 +8,22 @@ describe "Reservation" do
       @reservation = Hotel::Reservation.new(1, @start_date, @end_date)
     end
 
-    it "create an instance of Reservation" do
+    it "creates an instance of Reservation" do
       expect(@reservation).must_be_kind_of Hotel::Reservation
+    end
+  end
+
+  describe "#cost" do
+    before do
+      @start_date = '2019-02-03'
+      @end_date = '2019-02-04'
+      @reservation = Hotel::Reservation.new(1, @start_date, @end_date)
+      @second_reservation = Hotel::Reservation.new(2, '2019-02-03', '2019-02-05')
+    end
+
+    it "gives the cost of a reservation" do
+      expect(@reservation.cost).must_equal 200
+      expect(@second_reservation.cost).must_equal 400
     end
   end
 end
