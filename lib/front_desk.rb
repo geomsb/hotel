f3b87a8bb70025a0e57ca8553d35bab9c0b4ffe0
Georgina Sanchez Buenrostro <cpgeorgina.sanchez@gmail.com>
2019-09-07 15:49:23 -0700

    front_desk can reserve a room

--- lib/front_desk.rb
+++ lib/front_desk.rb
@@ -3,8 +3,14 @@ require_relative 'reservation'
 
 module Hotel
   class Front_Desk
-    def initialize(rooms)
-      @rooms = Room.create_all
+
+    def initialize(rooms: nil)
+      @rooms = rooms || Room.create_all
+    end
+
+    def reserve_room(start_date, end_date)
+      available_rooms = Room.available_rooms_by_date(@rooms, start_date, end_date)
+      return available_rooms[0].add_reservation(start_date, end_date)
     end
 
     # Before reserving a block I need to check if all the rooms are available for that specific range, then
@@ -12,27 +18,26 @@ module Hotel
 
     def reserve_and_create_a_block(rooms_block, start_date, end_date)
 
-      if rooms.length > 5
+      if rooms_block.length > 5
         raise ArgumentError.new("A block can contain a maximum of 5 rooms")
       end
 
       available_block_rooms = []
 
-      rooms.each do |room|
+      rooms_block.each do |room|
         if room.available_date?(start_date, end_date) == true
-          available_block_rooms += room
+          available_block_rooms << room
         else
           raise StandardError.new("The room #{room.room_num} is not available for that dates")
-      end
-
-      if available_block_rooms.length == rooms.length
-        new_block = Hotel::Hotel_block.new(rooms, start_date, end_date)
-        available_block_rooms.each do |room|
-          room.add_reservation(@start_date, @end_date)
         end
       end
 
-    return new_block
+      if available_block_rooms.length == rooms_block.length
+        new_block = Hotel::Hotel_block.new(available_block_rooms, start_date, end_date)
+        available_block_rooms.each do |room|
+          room.add_reservation(start_date, end_date)
+        end
+      end
     end
   end
 end
--- test/front_desk_test.rb
+++ test/front_desk_test.rb
@@ -1,17 +1,35 @@
 require_relative "test_helper"
 
-describe "Hotel_block" do
+describe "Front_desk" do
   describe "reserve_and_create_a_block" do
     before do
       @start_date = '2019-02-03'
       @end_date = '2019-02-04'
-      @front_desk = Hotel::Front_Desk.new(rooms)
+      @front_desk = Hotel::Front_Desk.new
       @rooms_block = [@front_desk[0], @front_desk[1], @front_desk[2]]
     end
 
     it "checks if all the rooms are available for that specific range and it reserves and creates the block." do
       new_block = @front_desk.reserve_a_block(@rooms_block, @start_date, @end_date)
     end
+  end
 
+  describe "reserve_room" do
+    before do
+      @start_date = '2019-02-03'
+      @end_date = '2019-02-04'
+      @front_desk = Hotel::Front_Desk.new
+    end
+
+    it "should determine which room to use for the reservation" do
+      expect(@front_desk.reserve_room(@start_date, @end_date)).must_be_kind_of Hotel::Reservation
+    end
+
+    it "reserves a rooms" do
+      room = Hotel::Room.new(1)
+      front_desk = Hotel::Front_Desk.new(rooms: [room])
+      reservation = front_desk.reserve_room(@start_date, @end_date)
+      expect(room.reservations[0]).must_equal reservation
+    end
   end
 end
